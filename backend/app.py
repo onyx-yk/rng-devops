bdaa27b32cc695c442b3a29d0073c5696d9228a9
onyx-yk <thinkingcustard@gmail.com>
2025-08-20 21:26:39 +0530

    changed py

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import random
 
 app = Flask(__name__)
+current_number = random.randint(1, 100)
 
 @app.route('/random')
 def random_number():
-    return jsonify({"number": random.randint(1, 100)})
+    global current_number
+    if request.args.get('refresh') == 'true':
+        current_number = random.randint(1, 100)
+    
+    return jsonify({"number": current_number})
 
 @app.route('/')
 def home():
     return "RNG API is running!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
